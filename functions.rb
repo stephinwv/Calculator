c9f7d7233c4d022214b92294d80bea56ef4e7fdd
stephinwv <thekingdomseeker@aol.com>
2017-07-28 13:30:16 -0400

    Add files via upload
    Finished, added italics and bold print, along with a new break after radio buttons.

--- functions.rb
+++ functions.rb
@@ -1,4 +1,12 @@
-def functions (num1,num2)
-	
-	
+def calc(math, num1, num2)
+	if math == "add"
+		sum = num1.to_i + num2.to_i
+	elsif math == 'subtract'
+		sum = num1.to_i - num2.to_i
+	elsif math == 'multiply'
+		sum = num1.to_i * num2.to_i
+	elsif math == 'divide'
+		sum = num1.to_i / num2.to_i
+	end
+	sum.to_s
 end
